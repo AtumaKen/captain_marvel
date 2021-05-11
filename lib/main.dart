8b22022b9ccd229f0c2b4b7ba2828612ffda8fe3
Kenoly <atumaken@gmail.com>
2021-05-11 09:39:00 +0100

    removed providers.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:captain_marvel/providers/appearance_provider.dart';
 import 'package:captain_marvel/providers/comic_provider.dart';
 import 'package:captain_marvel/screens/page_view_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 import 'screens/comics_screen.dart';
 
@@ -14,18 +13,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-        providers: [
-          ChangeNotifierProvider.value(
-            value: ComicProvider(),
-          ),
-          ChangeNotifierProvider.value(value: AppearanceProvider())
-        ],
-        child: MaterialApp(
-          title: 'Flutter Demo',
-          theme: ThemeData(primaryColor: Colors.black),
-          home: MainScreen(),
-        ),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primaryColor: Colors.black),
+      home: MainScreen(),
     );
   }
 }
