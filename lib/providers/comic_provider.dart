e0af46eaa2c4dcfa0396cc1ec154207b0f4d1812
AtumaKen <atumaken@gmail.com>
2021-05-11 09:56:24 +0100

    fixed comic_provider.dart on this branch

--- lib/providers/comic_provider.dart
+++ lib/providers/comic_provider.dart
@@ -3,15 +3,15 @@ import 'package:flutter/cupertino.dart';
 
 import '../service/api_service.dart';
 
-enum LoadingStatus { DONE, LOADING }
+enum LoadingStatus { DONE, LOADING, ERROR, END }
 
 class ComicProvider with ChangeNotifier {
   late ApiService _apiService;
-
+  LoadingStatus _loadingStatus = LoadingStatus.DONE;
   late List<Comic> _comics;
-
+  getLoadingStatus() => _loadingStatus;
   List<Comic> get comics => _comics;
-  int _totalPages = 0;
+  static int _totalPages = 0;
 
   ComicProvider() {
     initStreams();
@@ -23,14 +23,35 @@ class ComicProvider with ChangeNotifier {
     _comics = [];
   }
 
-  fetchData(int offset) async {
-    List<Comic> coms = await _apiService.getComics(offset);
-    if (coms.isEmpty) {
-      _totalPages = _comics.length;
-      _comics = coms;
-    } else {
-      _comics.addAll(coms);
+  void fetchData(int offset) async {
+    try {
+      List<Comic> localComics = await _apiService.getComics(offset);
+      if (localComics.isEmpty) {
+        _totalPages = _comics.length;
+        _comics = localComics;
+        setLoadingStatus(LoadingStatus.DONE);
+      } else if(_totalPages <= ApiService.totalComics) {
+        _loadMore(localComics);
+      }
+      else setLoadingStatus(LoadingStatus.END);
+    } catch(e){
+      setLoadingStatus(LoadingStatus.ERROR);
+      print(e.toString());
     }
+  }
+
+  void _loadMore(List<Comic> localComics) {
+    setLoadingStatus(LoadingStatus.LOADING);
+    _comics.addAll(localComics);
+    _totalPages += comics.length;
+    setLoadingStatus(LoadingStatus.DONE);
+  }
+
+
+
+  void setLoadingStatus(LoadingStatus loadingStatus){
+    _loadingStatus = loadingStatus;
     notifyListeners();
   }
+
 }
